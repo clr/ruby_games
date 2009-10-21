2877b9e9aae2b91b0ba8704ecdfb92384a15361c
clr <clr@port49.com>
2009-10-21 16:44:54 -0400

    -- update --

--- 004/spec/models/problem_spec.rb
+++ 004/spec/models/problem_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Problem do
 
   it "should take a number and spit back it's prime roots" do
-    factors = Primer.root( 331803648551 )
+    factors = Problem.root( 331803648551 )
     factors.should ==[ 576019, 576029 ]
   end 
 
